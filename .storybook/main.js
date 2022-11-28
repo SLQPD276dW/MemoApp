681da3ce7bc92f6f51608864fce166f390705ad0
SLQPD276dW <51816533+SLQPD276dW@users.noreply.github.com>
2022-11-28 17:10:28 +0900

    upgrade: storybookを最新版にアップグレード

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,25 +1,20 @@
 const path = require('path');
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '@storybook/addon-interactions',
-    {
-      name: '@storybook/addon-postcss',
-      options: {
-        postcssLoaderOptions: {
-          implementation: require('postcss'),
-        },
-      },
-    },
-  ],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions', {
+    name: '@storybook/addon-postcss',
+    options: {
+      postcssLoaderOptions: {
+        implementation: require('postcss')
+      }
+    }
+  }],
   framework: {
     //"name": "@storybook/react-webpack5",
     name: '@storybook/react-vite',
-    options: {},
+    options: {}
   },
   docsPage: {
-    docs: 'automatic',
-  },
+    docs: 'automatic'
+  }
 };
